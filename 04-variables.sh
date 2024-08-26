8fc50f5471c371ac1e731e55e29b0fcd0125783f
Firdose <pfirdose1988@gmail.com>
2024-08-26 10:41:53 +0530

    my second file

--- 04-variables.sh
+++ 04-variables.sh
@@ -8,4 +8,3 @@ echo "${PERSON2} : : Hello $PERSON1 iam doing good
 echo "$PERSON1:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
 echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
-echo "${PERSON2}:: I started learning Shell Script $PERSON1"

edd7b2f3608f17d8b086bae7e22362f2ef9e0d87
Firdose <pfirdose1988@gmail.com>
2024-08-26 10:30:46 +0530

    my second file

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,4 +6,4 @@ echo "$PERSON1:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
 echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
 echo "${PERSON2}:: I started learning Shell Script $PERSON1"
-echo "$PERSON1 :: that sounds good
+echo "$PERSON1 :: that sounds good"

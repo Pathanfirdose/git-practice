0689f048b59344c0cb1d27a8fbd6ff05b2fe3b67
Firdose <pfirdose1988@gmail.com>
2024-08-26 10:43:03 +0530

    my second file

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1 :: Hi ${PERSON2}, how are you"
+echo "$PERSON1 :: Hi ${PERSON2},how are you"
 echo "${PERSON2} : : Hello $PERSON1 iam doing good
 echo "$PERSON1:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"

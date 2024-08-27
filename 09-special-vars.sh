18851b423d424506a13624285414623fe53138ee
Firdose <pfirdose1988@gmail.com>
2024-08-27 14:22:05 +0530

    my second file

---
+++ 09-special-vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "All the variables passed to the script :: $@"
+echo "Number of variables passed :: $#"
+echo "Current working directory:: $PWD"
+echo "Home directory of current user:: $HOME"

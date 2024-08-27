9392ac8f56a4dcd7a3010ea62ec68655885c5849
Firdose <pfirdose1988@gmail.com>
2024-08-27 14:27:09 +0530

    my second file

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,3 +5,5 @@ echo "Number of variables passed :: $#"
 echo "Current working directory:: $PWD"
 echo "Home directory of current user:: $HOME"
 echo "PID of script running now ::$$"
+sleep 100 &
+echo "PID of last process run in background ::$!"
